fd7633a43fbcd3ad002322581daea4c847d7ff0f
Daniel Nordby <nordbydaniel@gmail.com>
2020-03-15 21:22:59 -0400

    updated for dynamic ref call

--- src/store/header/module.js
+++ src/store/header/module.js
@@ -20,14 +20,17 @@ const module = {
     },
 
     async getHeaderData(context) {
-      const url = 'https://bitterpenguin.prismic.io/api/v2/documents/search'
+      const access_token = 'MC5YbTZTS0JFQUFDSUFDSDhZ.77-9FxXvv71f77-977-977-977-9HO-_ve-_vTZGeO-_vWfvv73vv70A77-9Gu-_ve-_ve-_ve-_vSNOMe-_vWvvv70'
+      const refUrl = `https://bitterpenguin.prismic.io/api/v2?access_token=${access_token}`
+      const dataUrl = 'https://bitterpenguin.prismic.io/api/v2/documents/search'
+      const reference = await Axios.get(refUrl).then((response) => { return response.data.refs.filter((ref) => ref.id === 'master')[0].ref }).catch(error => console.log(error))
       const params = {
-        ref: 'XmZtERMAACEARpPb',
-        access_token: 'MC5YbTZTS0JFQUFDSUFDSDhZ.77-9FxXvv71f77-977-977-977-9HO-_ve-_vTZGeO-_vWfvv73vv70A77-9Gu-_ve-_ve-_ve-_vSNOMe-_vWvvv70',
+        ref: `${reference}`,
+        access_token,
         q: '[[at(document.type, "mainnavigation")]]',
         format: 'json'
       }
-      return Axios.get(url, {params})
+      return Axios.get(dataUrl, {params})
         .then((reponse) => {
           const headerData = reponse.data.results[0].data.linklist
           return context.commit('setHeader', headerData)
