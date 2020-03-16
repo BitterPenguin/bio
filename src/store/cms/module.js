a877a6d2ff18f1830162870730abb9b8826bf687
Daniel Nordby <nordbydaniel@gmail.com>
2020-03-15 23:04:54 -0400

    abstracted CMS layer

--- src/store/cms/module.js
+++ src/store/cms/module.js
@@ -32,17 +32,14 @@ const cms = {
       const nav = await context.dispatch('getNav')
       context.commit('setHeader', nav)
     },
-  
-  
+
     async getMasterRef() {
       const url = `https://bitterpenguin.prismic.io/api/v2?access_token=${this.state.cms.access_token}`
       return await Axios.get(url)
                     .then((response) => { return response.data.refs.filter((ref) => ref.id === 'master')[0].ref })
                     .catch(error => console.log(error))
-      
     },
-  
-  
+
     async getNav() {
       const url = 'https://bitterpenguin.prismic.io/api/v2/documents/search'
       const params = {
